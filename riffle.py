8857951c29179450ac93520f53d7d2b287ff93ee
Sam Rapier <samrapier@live.co.uk>
2018-10-22 20:42:50 +0100

    Added the quality functions

--- riffle.py
+++ riffle.py
@@ -23,6 +23,36 @@ def riffle(L, N):
     
     return L
 
+def quality(L):
+    qualityCount = 0
+
+    for count in range(len(L) - 1):
+        if(L[count + 1] > L[count]):
+            qualityCount = qualityCount + 1
+    
+    qualityScore = qualityCount / (len(L)-1)
+    # print(qualityCount)
+    return qualityScore
+
+def average_quality(N, trials):
+    testList = list(range(50))
+    totalQuality = 0
+    averageQuality = 0
+    for i in range(trials):
+        totalQuality = totalQuality + quality(riffle(testList, N))
+
+    averageQuality = totalQuality / trials
+    print(averageQuality)
+
+
+
+
 
 if __name__ == "__main__":
-    print(riffle(list(range(22)), 30))
+    # print(riffle(list(range(22)), 30))
+    # print(quality(list(range(50))))
+    print(quality(riffle(list(range(50)), 30)))
+    # names = ['alpha', 'beta', 'gamma', 'delta', 'epsilon', 'zeta', 'eta', 'theta', 'iota', 'kappa', 'lambda', 'mu']
+    # print(riffle_once(names))
+
+    average_quality(2, 30)
